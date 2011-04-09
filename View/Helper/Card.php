24033f4a0265d1b475e004b8a846460e25dafe19
Aaron Cox <aaron.cox@greymass.com>
2011-04-08 23:48:57 -0500

    added blank cards and some new logic to add custom content

--- View/Helper/Card.php
+++ View/Helper/Card.php
@@ -33,13 +33,19 @@ class EpicDb_View_Helper_Card extends MW_View_Helper_HtmlTag
 		}
 		return $record->getIcon();
 	}
-	public function cardDetails($record, $extra = null) {
+	public function cardDetails($record, $params = null) {
 		$details = '';
 		$details .= $this->htmlTag("h2", array('class' => 'text-large'), $this->link($record));
-		foreach($record->cardProperties($this->view) as $qualifier => $content) {
-			$details .= $this->_detail($qualifier, $content);
+		if(isset($params['content'])) {
+			foreach($params['content'] as $qualifier => $content) {
+				$details .= $this->_detail($qualifier, $content);
+			}						
+		} else {
+			foreach($record->cardProperties($this->view) as $qualifier => $content) {
+				$details .= $this->_detail($qualifier, $content);
+			}			
 		}
-		if($extra) $details .= $this->addExtra($extra);
+		if(isset($params['extra'])) $details .= $this->addExtra($params['extra']);
 		return $details;
 	}
 	public function cardScore($record) {
@@ -60,7 +66,16 @@ class EpicDb_View_Helper_Card extends MW_View_Helper_HtmlTag
 			$title."".$record->reputation
 		);
 	}
+	public function unknownCard($params) {
+		return $this->htmlTag("div", array('class' => 'inline-flow db-card transparent-bg rounded '.$params['class']), 
+			$this->htmlTag("div", array('class' => 'record-icon inline-flow rounded'), 
+				$this->htmlTag("img", array('src' => '/images/icons/unknown.jpg'))
+			)."".
+			$this->htmlTag("div", array('class' => 'record-info inline-flow'), $this->htmlTag("h2", array('class' => 'text-large'), 'Missing Account'))
+		);		
+	}
 	public function card($record, $params = array()) {
+		if(!$record) return $this->unknownCard($params);
 		if(!isset($params['extra'])) $params['extra'] = null;
 		if(!isset($params['class'])) $params['class'] = null;		
 		if($record->characters && $character = $record->characters->getPrimary()) {
@@ -72,7 +87,7 @@ class EpicDb_View_Helper_Card extends MW_View_Helper_HtmlTag
 				$this->cardScore($record)."".
 				$this->link($record, array("text" => $this->htmlTag("img", array('src' => $this->getIcon($record)))))				
 			)."".
-			$this->htmlTag("div", array('class' => 'record-info inline-flow'), $this->cardDetails($record, $params['extra']))
+			$this->htmlTag("div", array('class' => 'record-info inline-flow'), $this->cardDetails($record, $params))
 		);
 	}
 }
