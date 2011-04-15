3d0a37f75f15e6d3517af1d70d10610edc4e67ef
gnarf <gnarf@gnarf.net>
2011-04-15 18:16:40 -0500

    Moved the Diamonds out of the Card and into the ProfileLink

--- View/Helper/ProfileLink.php
+++ View/Helper/ProfileLink.php
@@ -10,6 +10,25 @@
  **/
 class EpicDb_View_Helper_ProfileLink extends MW_View_Helper_HtmlTag
 {
+	public function getDiamonds($profile)
+	{
+		$user = $profile->user;
+		$admin = $moderator = null;
+		if($user instanceOf MW_Auth_Mongo_Role) {
+			$admin = $user->isMember(MW_Auth_Group_Super::getInstance());
+			// if (!$admin) {
+			// 	$moderator = $user->isMember(R2Db_Auth_Group_Moderators::getInstance());
+			// }
+		}
+		$title = '';
+		if($admin || $moderator) {
+			$title .= $this->htmlTag("span", array(
+				"title" => $admin ? "Site Administrator" : "Moderator"
+			), $admin ? "&diams;&diams;" : "&diams;");
+		}
+		return $title;
+	}
+	
 	public function profileLink($profile, $params = array()) {
 		$tooltip = true;
 		if(isset($params['tooltip']) && $params['tooltip'] == false) {
@@ -18,7 +37,7 @@ class EpicDb_View_Helper_ProfileLink extends MW_View_Helper_HtmlTag
 		if (!is_object($profile)) {
 			return '';
 		}
-		$text = $profile->name;
+		$text = $this->getDiamonds($profile).$profile->name;
 		if(isset($params['text'])) {
 			$text = $params['text'];
 		}
