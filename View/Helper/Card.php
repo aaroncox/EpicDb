b7a0c75735f644ac3e04c25973e1284d1eb2fd2d
Aaron Cox <aaron.cox@greymass.com>
2011-04-15 11:03:45 -0500

    Added ability to add classes onto the ICON (coloring?)

--- View/Helper/Card.php
+++ View/Helper/Card.php
@@ -67,7 +67,7 @@ class EpicDb_View_Helper_Card extends MW_View_Helper_HtmlTag
 		);
 	}
 	public function unknownCard($params) {
-		return $this->htmlTag("div", array('class' => 'inline-flow db-card transparent-bg rounded '.$params['class']), 
+		return $this->htmlTag("div", array('class' => 'inline-flow db-card rounded '.$params['class']), 
 			$this->htmlTag("div", array('class' => 'record-icon inline-flow rounded'), 
 				$this->htmlTag("img", array('src' => '/images/icons/unknown.jpg'))
 			)."".
@@ -78,12 +78,13 @@ class EpicDb_View_Helper_Card extends MW_View_Helper_HtmlTag
 		if(!$record) return $this->unknownCard($params);
 		if(!isset($params['extra'])) $params['extra'] = null;
 		if(!isset($params['class'])) $params['class'] = null;
+		if(!isset($params['iconClass'])) $params['iconClass'] = null;
 		if($record instanceOf EpicAdvice_Mongo_Profile_User && $record->characters && $character = $record->characters->getPrimary()) {
 			$params['class'] .= " faction-".$character->faction;
 		}
 		if(!$record instanceOf EpicDb_Interface_Cardable) return '';
-		return $this->htmlTag("div", array('class' => 'inline-flow db-card transparent-bg rounded '.$params['class']), 
-			$this->htmlTag("div", array('class' => 'record-icon inline-flow rounded'), 
+		return $this->htmlTag("div", array('class' => 'inline-flow db-card rounded '.$params['class']), 
+			$this->htmlTag("div", array('class' => 'record-icon inline-flow rounded '.$params['iconClass']), 
 				$this->cardScore($record)."".
 				$this->link($record, array("text" => $this->htmlTag("img", array('src' => $this->getIcon($record), 'alt' => $record->name))))				
 			)."".
