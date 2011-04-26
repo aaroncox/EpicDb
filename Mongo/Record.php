76fa2f8f1ec57618ea99a3832d739b54c0d474de
Aaron Cox <aaron.cox@greymass.com>
2011-04-26 15:53:18 -0500

    Pattern for loading admin forms on records

--- Mongo/Record.php
+++ Mongo/Record.php
@@ -58,4 +58,11 @@ class EpicDb_Mongo_Record extends MW_Mongo_Document implements EpicDb_Interface_
 	public static function getFollowers($record, $limit = 9999) {
 		return $return = EpicDb_Mongo::db('profile')->fetchAll(array("following" => $record->createReference()), array(), $limit);
 	}
+	
+	public function getAdminForms() {
+		$forms = array();
+		$forms['changeIcon'] = new EpicDb_Form_Record_Icon(array("record" => $this, "title" => "Change Icon", "description" => "Change the Icon that this record uses, image should be an 80x80 jpg/png/gif."));
+		return $forms;
+	}
+	
 } // END class EpicDb_Mongo_Record
