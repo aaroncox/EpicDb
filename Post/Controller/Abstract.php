6fc11e8801ba85ddcf92a236e3bf0f282c748c9b
Aaron Cox <aaron.cox@greymass.com>
2011-05-16 16:35:08 -0500

    Merged this nasty action (needs abstraction) into the abstract itself for R2's usage.

--- Post/Controller/Abstract.php
+++ Post/Controller/Abstract.php
@@ -10,6 +10,102 @@
  **/
 class EpicDb_Post_Controller_Abstract extends MW_Controller_Action
 {
+	public function questionsAction() {
+
+		$request = $this->getRequest();
+		$auth = $this->_helper->auth;
+
+		// var_dump($auth->getUser()->roles[0]->export());
+
+		if ($request->getParam('ask')) {
+			if(!$auth->getUser()) {
+				throw new MW_Auth_Exception("You must be logged in to post a question.");
+			}
+			$question = EpicDb_Mongo::newDoc('question');
+			$this->view->form = $form = $question->getEditForm();
+			$this->_helper->viewRenderer('ask');
+			$this->_handleMWForm($form, 'ask');
+
+		} else if($question = $request->getParam('id')) {
+			$query = array(
+				'id' => (int) $this->getRequest()->getParam('id')
+			);
+			$question = $this->view->question = EpicDb_Mongo::db('question')->fetchOne($query);
+			if (!$question) {
+				$answer = EpicDb_Mongo::db('answer')->fetchOne($query);
+				if (!$answer) throw new MW_Controller_404Exeption('Invalid Question ID');
+				$question = $answer->_parent;
+				$slug = new MW_Filter_Slug();
+				return $this->_redirect($this->view->url(array(
+					'id' => $question->id,
+					'slug' => $slug->filter($question->title),
+					'answer' => $answer->id,
+				)).'#answer-'.$answer->id);
+			}
+			$this->_post = $question;
+			$this->getRequest()->setParam('post', $question);
+			if (!$this->getRequest()->getParam("format")) {
+				$this->_forward('view');
+			}
+		} else {
+			$query = array();
+			if($this->view->tag = $tag = $request->getParam("tagged")) {
+				$queryData = EpicDb_Search::getInstance()->parseQueryString($tag);
+				$query = $queryData['query'];
+				$tags = $queryData['terms']['tagged'];
+				$query["_type"] = "question";
+				$tagLinks = array();
+				foreach ($tags as $tag) {
+					if ($tag instanceOf EpicDb_Mongo_Record) {
+						$tagLinks[] = $this->view->recordLink($tag)."";
+					} else {
+						$tagLinks[] = $this->view->profileLink($tag)."";
+					}
+				}
+				$this->view->title = "Recent Questions Tagged ".implode(", ", $tagLinks);
+
+				$this->view->headLink()->appendAlternate(
+					$this->view->url(array(
+						'tagged' => $request->getParam('tagged')
+					),'se_feeds_tag',true),
+					"application/rss+xml",
+					$this->view->title
+				);
+			} else {
+				$this->view->title = "Recent Questions";
+				$this->view->headLink()->appendAlternate(
+					$this->view->url(array(),'se_feeds',true),
+					"application/rss+xml",
+					$this->view->title
+				);
+			}
+			switch($this->getRequest()->getParam("sort")) {
+				case "highest-voted":
+					$sort = array('votes.score' => -1, '_created' => -1);
+					break;
+				case "lowest-voted":
+					$sort = array('votes.score' => 1, '_created' => -1);
+					break;
+				case "oldest":
+					$sort = array('_created' => 1);
+					break;
+				case "newest":
+				default:
+					$sort = array('touched' => -1, '_created' => -1);
+					break;
+			}
+			
+			$questions = EpicDb_Mongo::db('question')->fetchAll($query, $sort);
+
+			$paginator = Zend_Paginator::factory($questions);
+			$paginator->setCurrentPageNumber($this->getRequest()->getParam('page', 1));
+
+			$this->view->questions = $paginator;
+
+
+			$this->view->popularTags = array();// EpicDb_Mongo_Post::getTagsByUsage();
+		}
+	}
 	public function postJson() {
 		if($this->_request->isXmlHttpRequest()) {
 			$this->getResponse()->setHeader('Content-type', 'application/json');
