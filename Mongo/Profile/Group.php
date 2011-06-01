a159bb4ed46f96db66a4259d061eccb4949177cf
Aaron Cox <aaron.cox@greymass.com>
2011-06-01 16:30:42 -0500

    Edit Type for Groups.

--- Mongo/Profile/Group.php
+++ Mongo/Profile/Group.php
@@ -10,7 +10,10 @@
  **/
 class EpicDb_Mongo_Profile_Group extends EpicDb_Mongo_Profile
 {
+	public $summaryHelper = 'groupSummary';
 	protected static $_documentType = 'group';
+	protected static $_editForm = 'EpicDb_Form_Profile_Group';
+	
 	 /**
    * The Privileges granted to group members
    *
