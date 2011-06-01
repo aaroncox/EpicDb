67f1fd2cbbd648b224b6c2b10ade9cf3e5735400
Aaron Cox <aaron.cox@greymass.com>
2011-06-01 16:31:08 -0500

    SummaryHelper for Guilds and Edit Form for Guilds.

---
+++ Mongo/Profile/Group/Guild.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * EpicDb_Mongo_Record_Group_Website
+ *
+ * @author Aaron Cox <aaronc@fmanet.org>
+ **/
+class EpicDb_Mongo_Profile_Group_Guild extends EpicDb_Mongo_Profile_Group
+{
+	public $summaryHelper = 'guildSummary';
+	
+	protected static $_documentType = "guild";
+	protected static $_editForm = 'EpicDb_Form_Profile_Group_Guild';
+	
+	public function getParentResource() {
+		return new EpicDb_Auth_Resource_Profile();
+	}
+}
