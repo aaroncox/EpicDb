471449f5093b5a0b98808a4685fa071a815f679e
Aaron Cox <aaron.cox@greymass.com>
2011-06-14 15:29:51 -0500

    Probably a better way to do this but I wanted to get a concept in place. Special tagging mechanism for answers. If you tag an answer with a record, it's considered to be in an 'answer-tag' category being part of the answer, and displayed inline with the answer.

--- Form/Post/Question/Answer.php
+++ Form/Post/Question/Answer.php
@@ -21,9 +21,49 @@ class EpicDb_Form_Post_Question_Answer extends EpicDb_Form_Post
 	public function init()
 	{
 		parent::init();
-		$this->removeElement('tags');
+		$post = $this->getPost();
+		if(!R2Db_Auth::getInstance()->getUser()->isMember(MW_Auth_Group_Super::getInstance())) {
+			$this->tags->removeElement();
+		} else {
+			$this->tags->setLabel('Tag records as the answer...')->setDescription('Does an item, quest or NPC in the database answer this question? Tag it!');
+			$this->setDefaults(array(
+				"tags" => $post->tags->getTags('answer-tag'),
+			));
+		}
 		$this->source->setLabel("Your Answer")->setDescription("Do you have the answer to this question? Post your answer to earn achievements and reputation on EpicAdvice.com!");
 		$this->setButtons(array("save" => "Post Answer"));
-		
+	}
+	public function save() {
+		$me = EpicDb_Auth::getInstance()->getUserProfile();
+		$post = $this->getPost();
+		if(!$this->_isNew) {
+			$post->bump($me);
+			EpicDb_Mongo_Revision::makeEditFor($post, $this->reason->getValue());
+		} else {
+			$post->_created = time();
+		}
+		if($this->source) {
+			$post->source = $this->source->getValue();
+			$post->body = $this->source->getRenderedValue();
+		}
+		$filter = new EpicDb_Filter_TagJSON();
+		if ($this->tags) {
+			$post->tags->setTags($filter->toArray($this->tags->getValue()),'answer-tag');
+		}
+		if($post->_parent && $post->_parent->export() != array()) {
+			$parentAuthor = $post->_parent->tags->getTag('author')?:$post->_parent->tags->getTag('source');
+			if($parentAuthor) {
+				$post->tags->tag($parentAuthor, 'responding-to');
+				$post->_parent->bump($me);
+			}
+		}
+		return $post->save();
+	}
+	public function process($data) {
+		if($this->isValid($data)) {
+			$this->save();
+			return true;
+		}
+		return false;
 	}
 } // END class EpicDb_Form_Post_Question_Answer
