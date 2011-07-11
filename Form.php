cb032587504cf349ae41aa0ab6af3f0a86ea49cc
Aaron Cox <aaron.cox@greymass.com>
2011-07-11 14:47:41 -0500

    Added the description/title to all record forms.

--- Form.php
+++ Form.php
@@ -1,5 +1,26 @@
 <?php
 class EpicDb_Form extends MW_Form {
+	protected $_description = "Empty Description";
+	protected $_title = "Empty Title";
+
+	public function setDescription($description) {
+		$this->_description = $description;
+		return $this;
+	}
+
+	public function getDescription() {
+		return $this->_description;
+	}
+
+	public function setTitle($title) {
+		$this->_title = $title;
+		return $this;
+	}
+
+	public function getTitle() {
+		return $this->_title;
+	}
+	
   /**
    * adds the MW_Form prefix path
    *
