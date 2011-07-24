7b84768f26863776b7285758fa1d4c290c4139cf
Corey Frang <gnarf@gnarf.net>
2011-07-23 21:19:55 -0500

    Temporary fix for an issue, we need to make this "admins" and "members" auto detect type, its not really even used right now tho

--- Mongo/Profile/Group.php
+++ Mongo/Profile/Group.php
@@ -3,7 +3,7 @@
  * EpicDb_Mongo_Profile_Group
  *
  * undocumented class
- * 
+ *
  * @author Aaron Cox <aaronc@fmanet.org>
  * @param undocumented class
  * @package undocumented class
@@ -14,169 +14,170 @@ class EpicDb_Mongo_Profile_Group extends EpicDb_Mongo_Profile
 	public $contextHelper = 'groupContext';
 	protected static $_documentType = 'group';
 	protected static $_editForm = 'EpicDb_Form_Profile_Group';
-	
+
 	 /**
-   * The Privileges granted to group members
-   *
-   * @var array
-   **/
-  protected $_memberPrivileges = array("view", "post");
+	 * The Privileges granted to group members
+	 *
+	 * @var array
+	 **/
+	protected $_memberPrivileges = array("view", "post");
 
-  // override requirements
-  public function __construct($data = array(), $config = array()) {
-    $this->addRequirements(array(
-      '_owner' => array('Document:MW_Auth_Mongo_Role', 'AsReference', 'Required'),
-      '_memberRole' => array('Document:MW_Auth_Mongo_Role', 'AsReference'),
-      '_adminRole' => array('Document:MW_Auth_Mongo_Role', 'AsReference'),
-      'admins' => array('DocumentSet'),
-      'admins.$' => array('Document:EpicDb_Mongo_Profile', 'AsReference'),		
-      'members' => array('DocumentSet'),
-      'members.$' => array('Document:EpicDb_Mongo_Profile', 'AsReference'),
-    ));
-    return parent::__construct( $data, $config );
-  }
+	// override requirements
+	public function __construct($data = array(), $config = array()) {
+		$this->addRequirements(array(
+			'_owner' => array('Document:MW_Auth_Mongo_Role', 'AsReference', 'Required'),
+			'_memberRole' => array('Document:MW_Auth_Mongo_Role', 'AsReference'),
+			'_adminRole' => array('Document:MW_Auth_Mongo_Role', 'AsReference'),
+			'admins' => array('DocumentSet'),
+			'admins.$' => array('Document:EpicDb_Mongo_Profile_User', 'AsReference'),
+			'members' => array('DocumentSet'),
+			'members.$' => array('Document:EpicDb_Mongo_Profile_User', 'AsReference'),
+		));
+		return parent::__construct( $data, $config );
+	}
 
-  /**
-   * Helper functions - isAdmin() and isMember() check the currently logged in user
-   *
-   * @return boolean
-   * @author Corey Frang
-   **/
-  public function isAdmin($user = null) {
+	/**
+	 * Helper functions - isAdmin() and isMember() check the currently logged in user
+	 *
+	 * @return boolean
+	 * @author Corey Frang
+	 **/
+	public function isAdmin($user = null) {
 		if( !$user ) {
-	    $user = MW_Auth::getInstance()->getUser();			
+			$user = MW_Auth::getInstance()->getUser();
 		}
-    if ( !$user ) {
-      return false;
-    }
-    return $user->isMember( $this->getAdminRole() );
-  }
-  public function isMember($user = null) {
+		if ( !$user ) {
+			return false;
+		}
+		return $user->isMember( $this->getAdminRole() );
+	}
+	public function isMember($user = null) {
 		if( !$user ) {
-	    $user = MW_Auth::getInstance()->getUser();			
+			$user = MW_Auth::getInstance()->getUser();
 		}
-    if( !$user ) {
-      return false;
-    }
-    return $user->isMember( $this->getMemberRole() );
-  }
+		if( !$user ) {
+			return false;
+		}
+		return $user->isMember( $this->getMemberRole() );
+	}
 
-  /**
-   * getAdminRole - Returns the MW_Auth_Mongo_Role that represents the "admins"
-   *
-   * @return MW_Auth_Mongo_Role
-   * @author Corey Frang
-   **/
-  public function getAdminRole()
-  {
-    if ( isset( $this->_adminRole ) ) {
-      return $this->_adminRole;
-    }
-    $data = array(
-      'type' => 'group',
-      'groupName' => $this->name." admins",
-      'groupDescription' => "Group administrators for group ".$this->id.": ".$this->name,
-    );
-    $role = new MW_Auth_Mongo_Role();
-    foreach ($data as $k=>$v) $role->$k = $v;
+	/**
+	 * getAdminRole - Returns the MW_Auth_Mongo_Role that represents the "admins"
+	 *
+	 * @return MW_Auth_Mongo_Role
+	 * @author Corey Frang
+	 **/
+	public function getAdminRole()
+	{
+		if ( isset( $this->_adminRole ) ) {
+			return $this->_adminRole;
+		}
+		$data = array(
+			'type' => 'group',
+			'groupName' => $this->name." admins",
+			'groupDescription' => "Group administrators for group ".$this->id.": ".$this->name,
+		);
+		$role = new MW_Auth_Mongo_Role();
+		foreach ($data as $k=>$v) $role->$k = $v;
 
-    // The Admin role is a member of the Members Role
-    $role->roles->addDocument( $this->getMemberRole() );
-    $role->save();
+		// The Admin role is a member of the Members Role
+		$role->roles->addDocument( $this->getMemberRole() );
+		$role->save();
 
-    // Grant all privileges on this item to the Admins
-    $this->grant( $role );
+		// Grant all privileges on this item to the Admins
+		$this->grant( $role );
 
-    foreach( $this->admins as $admin ) {
-      $user = $admin->user;
-      if ( $user ) {
-        $user->addGroup( $role );
-        $user->save();
-      }
-    }
+		foreach( $this->admins as $admin ) {
+			$admin = 
+			$user = $admin->user;
+			if ( $user ) {
+				$user->addGroup( $role );
+				$user->save();
+			}
+		}
 
-    // Save this new role onto the record
-    $this->_adminRole = $role;
-    $this->save();
-    return $role;
-  }
-  
-  /**
-   * getMemberRole() - Returns the MW_Auth_Mongo_Role that represents the "members" 
-   *
-   * @return void
-   * @author Corey Frang
-   **/
-  public function getMemberRole() {
-    if ( isset( $this->_memberRole ) ) {
-      return $this->_memberRole;
-    }
-    $data = array(
-      "type" => "group",
-      "groupName" => $this->name." members",
-      "groupDescription" => "Group Members for the group ".$this->name,
-    );
-    $role = new MW_Auth_Mongo_Role();
-    foreach ($data as $k=>$v) $role->$k = $v;
-    
-    $role->save();
-    // Grant the member role privileges on this object
-    $this->grant($role, $this->_memberPrivileges);
+		// Save this new role onto the record
+		$this->_adminRole = $role;
+		$this->save();
+		return $role;
+	}
 
-    // Retroactively grant membership to the members in the members documentset
-    foreach( $this->members as $member ) {
-      $user = $member->user;
-      if( $user ) {
-        $user->addGroup($role); 
-        $user->save();
-      }
-    }
+	/**
+	 * getMemberRole() - Returns the MW_Auth_Mongo_Role that represents the "members"
+	 *
+	 * @return void
+	 * @author Corey Frang
+	 **/
+	public function getMemberRole() {
+		if ( isset( $this->_memberRole ) ) {
+			return $this->_memberRole;
+		}
+		$data = array(
+			"type" => "group",
+			"groupName" => $this->name." members",
+			"groupDescription" => "Group Members for the group ".$this->name,
+		);
+		$role = new MW_Auth_Mongo_Role();
+		foreach ($data as $k=>$v) $role->$k = $v;
 
-    // Save this new role onto the record
-    $this->_memberRole = $role;
-    $this->save();
-    return $role;
-  }
-  
-  public function setMembership(EpicDb_Mongo_Profile $profile, $type)
-  {
-    $validTypes = array('member', 'admin', '');
-    if (!in_array($type, $validTypes)) {
-      throw new Exception('Unknown User Type, must be "member", "admin", or "" to remove');
-    }
+		$role->save();
+		// Grant the member role privileges on this object
+		$this->grant($role, $this->_memberPrivileges);
 
-    $user = $profile->user;
-    $updateQuery = array();
+		// Retroactively grant membership to the members in the members documentset
+		foreach( $this->members as $member ) {
+			$user = $member->user;
+			if( $user ) {
+				$user->addGroup($role);
+				$user->save();
+			}
+		}
 
-    foreach ($validTypes as $checkType) {
-      if (!$checkType) continue;
-      $typeSetKey = $checkType.'s';
-      $set = $this->$typeSetKey;
+		// Save this new role onto the record
+		$this->_memberRole = $role;
+		$this->save();
+		return $role;
+	}
 
-      if ($checkType == $type) {
-        $updateQuery['$addToSet'][$typeSetKey] = $profile->createReference();
-        $user->addGroup($this->getRole($checkType));
-      } else {
-        $updateQuery['$pull'][$typeSetKey] = $profile->createReference();
-        $user->removeGroup($this->getRole($checkType));
-      }
-    }
-    $user->save();
-    self::update(array('_id' => $this->_id), $updateQuery);
-    return true;
-  }
+	public function setMembership(EpicDb_Mongo_Profile $profile, $type)
+	{
+		$validTypes = array('member', 'admin', '');
+		if (!in_array($type, $validTypes)) {
+			throw new Exception('Unknown User Type, must be "member", "admin", or "" to remove');
+		}
 
-  /**
-   * shortcut function to get admin/member role
-   *
-   * @return MW_Auth_Mongo_Role
-   * @author Corey Frang
-   **/
-  public function getRole($type)
-  {
-    if ($type == 'admin') return $this->getAdminRole();
-    if ($type == 'member') return $this->getMemberRole();
-    throw new Exception('Unknown Role Type');
-  }
+		$user = $profile->user;
+		$updateQuery = array();
+
+		foreach ($validTypes as $checkType) {
+			if (!$checkType) continue;
+			$typeSetKey = $checkType.'s';
+			$set = $this->$typeSetKey;
+
+			if ($checkType == $type) {
+				$updateQuery['$addToSet'][$typeSetKey] = $profile->createReference();
+				$user->addGroup($this->getRole($checkType));
+			} else {
+				$updateQuery['$pull'][$typeSetKey] = $profile->createReference();
+				$user->removeGroup($this->getRole($checkType));
+			}
+		}
+		$user->save();
+		self::update(array('_id' => $this->_id), $updateQuery);
+		return true;
+	}
+
+	/**
+	 * shortcut function to get admin/member role
+	 *
+	 * @return MW_Auth_Mongo_Role
+	 * @author Corey Frang
+	 **/
+	public function getRole($type)
+	{
+		if ($type == 'admin') return $this->getAdminRole();
+		if ($type == 'member') return $this->getMemberRole();
+		throw new Exception('Unknown Role Type');
+	}
 
 } // END class EpicDb_Mongo_Profile_Group
