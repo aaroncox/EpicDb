1e2430e0831d268a52f6d7a1f0a648689e66b0c0
Aaron Cox <aaron.cox@greymass.com>
2011-08-01 16:38:21 -0500

    Review Application button.

--- View/Helper/GuildSummary.php
+++ View/Helper/GuildSummary.php
@@ -51,13 +51,15 @@ class EpicDb_View_Helper_GuildSummary extends MW_View_Helper_HtmlTag
 			));			
 		}
 		if($curUser = EpicDb_Auth::getInstance()->getUserProfile()) {
-			if($profile->hasApplied($curUser)) {
+			if($application = $profile->application($curUser)) {
 				if($profile->_groupType == "open" || $profile->_groupType == "closed") {
 					$buttons .= $this->view->button(array(
-						'action' => 'leave',
-						'profile' => $profile
-					), 'profile', true, array(
-						'text' => 'Remove Application',
+						'user' => $application->candidate->id,
+						'group' => $application->group->id,
+						'gtype' => $application->group->_type,
+						'action' => 'view',
+					), 'group_application', true, array(
+						'text' => 'Review Application',
 						'icon' => 'key',
 						'data-tooltip' => 'Your application is currently under review by the leaders of this '.$profile->_type.', if you\'d like to delete your application, click here.',
 					));				
