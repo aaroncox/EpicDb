639387c842d98f2b97c85439274470426953fe46
Aaron Cox <aaron.cox@greymass.com>
2011-09-13 15:04:40 -0500

    Changed "Group Description" to "Description"

--- View/Helper/GroupContext.php
+++ View/Helper/GroupContext.php
@@ -13,8 +13,7 @@ class EpicDb_View_Helper_GroupContext extends MW_View_Helper_HtmlTag
 	public function groupContext(EpicDb_Mongo_Profile $profile) {
 		$placeholder = $this->view->context();
 		$placeholder->prepend($this->view->tooltip($profile));
-		if($profile->description) $placeholder->widget($this->htmlTag("h3", array(), "Group Description")."".$this->htmlTag("p", array(), $profile->description));
+		if($profile->description) $placeholder->widget($this->htmlTag("h3", array(), "Description")."".$this->htmlTag("p", array(), $profile->description));
 		$placeholder->widget($this->view->render('./_context/recent-news-group.phtml'));
-		
 	}
 } // END class EpicDb_View_Helper_ProfileSummary
