685e2f379833eb370d0e55c9349eaf42b8b994ef
Aaron Cox <aaron.cox@greymass.com>
2011-09-26 13:19:36 -0500

    Logic to allow original poster to make it a community post, or a moderator on edit.

--- Form/Post/Question.php
+++ Form/Post/Question.php
@@ -34,19 +34,28 @@ class EpicDb_Form_Post_Question extends EpicDb_Form_Post
 				'size' => 80,
 				'description' => '120 character or less description of your question.'
 			));
-		$this->addElement("checkbox", "community", array(
-			'label' => 'Community Post?',
-			'description' => 'Designate this question as a "Community Post", for more information read the Q&A FAQ.',
-			'order' => 200,
-		));
 		$this->setButtons(array("save" => "Post"));
 		if(!$this->_isNew) {
 			$this->source->setLabel("The Question")->setDescription("Please be as descriptive as possible when asking your question, include as many details as possible, and don't hit people in the face with a wall of text in one huge paragraph.");
 			$this->setDefaults(array("title" => $question->title, "body" => $question->body, "parent" => $question->_parent->_id));
 			$this->setButtons(array("save" => "Post Question"));
+			// Add the Community Post button to the edit only if you're a moderator.
+			if(EpicDb_Auth::getInstance()->hasPrivilege(new EpicDb_Auth_Resource_Moderator())) {
+				$this->addElement("checkbox", "community", array(
+					'label' => 'Community Post?',
+					'description' => 'Designate this question as a "Community Post", for more information read the Q&A FAQ.',
+					'order' => 200,
+				));				
+			}
 		} else {
 			$this->source->setLabel("The Question")->setDescription("Please be as descriptive as possible when asking your question, include as many details as possible, and don't hit people in the face with a wall of text in one huge paragraph.");
 			$this->setButtons(array("save" => "Post Question"));
+			// If you are creating a post, you can specify if its a community post.
+			$this->addElement("checkbox", "community", array(
+				'label' => 'Community Post?',
+				'description' => 'Designate this question as a "Community Post", for more information read the Q&A FAQ.',
+				'order' => 200,
+			));
 		}
 		// var_dump($this->_elements); exit;
 	}
