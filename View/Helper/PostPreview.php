be17e1cc143b13cad0fba73e9fd3e4a03c0900d4
root <root@dev.(none)>
2011-10-18 20:13:38 -0500

    New function for post previewing articles w/ the more tag

---
+++ View/Helper/PostPreview.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * undocumented class
+ *
+ * @package default
+ * @author Aaron Cox
+ **/
+class EpicDb_View_Helper_PostPreview extends Zend_View_Helper_Abstract
+{
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author Aaron Cox
+	 **/
+	function postPreview($html, $preview = false)
+	{
+		if($preview) {
+			$partial = explode("<!--more-->", $html);
+			$html = $partial[0];
+		}
+		return $html;
+	}
+} // END class EpicDb_View_Helper_PostPreview extends Zend_View_Helper_Abstract
