c345367d977e3be0050dfc188dc6e5cee9f8baf4
Aaron Cox <aaron.cox@greymass.com>
2011-10-22 19:11:29 -0500

    Migrated functions from R2 to a controller abstract for further use

---
+++ Wiki/Controller/Abstract.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * undocumented class
+ *
+ * @package default
+ * @author Aaron Cox
+ **/
+class EpicDb_Wiki_Controller_Abstract extends MW_Controller_Action
+{
+	protected $_wiki = null;
+	protected $_record = null;
+	public function getWiki() {
+		if($id = $this->getRequest()->getParam("record")) {
+			$record = $this->_record = EpicDb_Mongo::db('record')->find(new MongoId($id));
+			if(!$type = $this->getRequest()->getParam("type")) {
+				$type = "new";
+			}
+			return $this->view->wiki = $this->_wiki = EpicDb_Mongo::db('wiki')->get($record, $type);
+		} else {
+			return EpicDb_Mongo::newDoc('wiki');
+		}
+	}
+	public function indexAction() {
+		if(MW_Auth::getInstance()->getUser()) {
+			if(EpicDb_Mongo::db('group')->fetchOne(array("id" => 10))->isMember() || MW_Auth::getInstance()->getUser()->isMember(MW_Auth_Group_Super::getInstance())) {		
+				$wiki = $this->getWiki();
+				$revision = $this->getRequest()->getParam("rev", false);
+				if($type = $this->getRequest()->getParam('type')) {
+					$form = $this->view->form = new EpicDb_Form_Wiki(array("wiki" => $wiki, "record" => $this->_record, "type" => $type, "rev" => $revision));													
+				} else {
+					$form = $this->view->form = new EpicDb_Form_Wiki(array("wiki" => $wiki, "record" => $this->_record, "rev" => $revision));								
+				}
+				$this->_handleMWForm($form, 'wiki');			
+			} else {
+				return $this->_redirect("/user/login");
+			}
+		}
+	}
+	public function revisionsAction() {
+		$this->view->wiki = $this->getWiki();
+	}
+} // END class EpicDb_Wiki_Controller_Abstract extends MW_Controller_Action
