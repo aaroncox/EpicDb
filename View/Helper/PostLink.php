1a2b3d0cb663a8ccdb70a86beeaef63d5aa9cb43
root <root@dev.(none)>
2011-10-22 20:24:06 -0500

    added hashes into post link

--- View/Helper/PostLink.php
+++ View/Helper/PostLink.php
@@ -19,6 +19,9 @@ class EpicDb_View_Helper_PostLink extends MW_View_Helper_HtmlTag
 			$textLimit = $params['text-limit'];
 		}
 		$hash = '';
+		if(isset($params['hash'])) {
+			$hash = $params['hash'];
+		}
 		$text = $post->title;
 		if(isset($params['text'])) {
 			$text = $params['text'];
