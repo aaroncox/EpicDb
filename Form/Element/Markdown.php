8f048dcedcb92e9d5e206d91db1df291ed9d78b5
Aaron Cox <aaron.cox@greymass.com>
2011-10-25 16:15:29 -0500

    This code might be better off soempalce, but it allows youtube linking!

--- Form/Element/Markdown.php
+++ Form/Element/Markdown.php
@@ -10,4 +10,14 @@
  **/
 class EpicDb_Form_Element_Markdown extends MW_Form_Element_Markdown {
 	protected $_purifyOptions = array(array("HTML.Nofollow", 1));
+	
+	public function getRenderedValue()
+	{
+		$html = parent::getRenderedValue();
+		$match = '/\{http:\/\/[\w.]*youtube.com\/watch\?v=([^}]+)\}/';
+		$replace = '<iframe width="620" height="348" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>';
+		$html = preg_replace($match, $replace, $html);
+		return $html;
+	}
+	
 } // END class R2Db_Form_Element_Markdown
